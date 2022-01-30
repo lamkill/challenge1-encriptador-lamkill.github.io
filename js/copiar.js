fcb7472eec35e174612e6470967dd32606a1694a
Luis Angel <lmenapuello@gmail.com>
2022-01-30 17:28:08 -0400

    .

--- js/copiar.js
+++ js/copiar.js
@@ -7,7 +7,3 @@ btnCopiar.addEventListener("click", function (event) {
   textoCopiar.select();
   document.execCommand("copy");
 })
-
-/*src="/Users/lmena/DesktopCursos-de-Alura/Challenge-One/Challenge-1-encriptadorjs/encriptar.js"></script>
-    <script src="/Users/lmena/DesktopCursos-de-Alura/Challenge-One/Challenge-1-encriptadorjs/desencriptar.js"></script>
-    <script src="/Users/lmena/DesktopCursos-de-Alura/Challenge-One/Challenge-1-encriptadorjs/copiar.js"></script>*/
